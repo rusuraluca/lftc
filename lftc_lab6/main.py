5877dd6f83d3d73afde6d850c24e9caef69aa324
Raluca Rusu <rr.ralucarusu@gmail.com>
2023-11-21 10:45:27 +0200

    Started lab 6

---
+++ lftc_lab6/main.py
@@ -0,0 +1,12 @@
+from grammar import Grammar
+
+g = Grammar.from_file('g1.txt')
+print(g)
+print(g.get_nonterminal_productions('S'))
+print(g.is_cfg())
+
+
+g = Grammar.from_file('g3.txt')
+print(g)
+print(g.get_nonterminal_productions('A'))
+print(g.is_cfg())

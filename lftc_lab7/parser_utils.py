eca49ba00f26d6919d3ebd6890f3cbe62fcd3bb2
Raluca Rusu <rr.ralucarusu@gmail.com>
2023-12-09 21:16:35 +0200

    Started lab 7

---
+++ lftc_lab7/parser_utils.py
@@ -0,0 +1,73 @@
+class CanonicalCollection:
+    def __init__(self, states=None, adjacency_list=None):
+        self.states = states or []
+        self.adjacency_list = adjacency_list or {}
+
+    def add_state(self, state):
+        self.states.append(state)
+
+    def get_states(self):
+        return self.states
+
+    def get_adjacency_list(self):
+        return self.adjacency_list
+
+    def __len__(self):
+        return len(self.states)
+
+    def __str__(self):
+        return '\n'.join([str(state) for state in self.states])
+
+
+class State:
+    def __init__(self, items):
+        self.items = items
+
+    def get_items(self):
+        return self.items
+
+    def get_symbols_succeeding_the_dot(self):
+        symbols = set()
+        for item in self.items:
+            if item.get_position_for_dot() < len(item.get_right_hand_side()):
+                symbols.add(item.get_right_hand_side()[item.get_position_for_dot()])
+        return symbols
+
+    def __str__(self):
+        items = '\n '.join([str(item) for item in self.items])
+        return f"State:\n {items}"
+
+    def __eq__(self, other):
+        return self.items == other.items
+
+    def __hash__(self):
+        return hash(tuple(self.items))
+
+
+class Item:
+    def __init__(self, left_hand_side, right_hand_side, position_for_dot):
+        self.left_hand_side = left_hand_side
+        self.right_hand_side = right_hand_side
+        self.position_for_dot = position_for_dot
+
+    def get_left_hand_side(self):
+        return self.left_hand_side
+
+    def get_right_hand_side(self):
+        return self.right_hand_side
+
+    def get_position_for_dot(self):
+        return self.position_for_dot
+
+    def __str__(self):
+        return f"Item: {self.left_hand_side} -> {self.right_hand_side}, dot - {self.position_for_dot}"
+
+    def __eq__(self, other):
+        return (
+            self.left_hand_side == other.left_hand_side
+            and self.right_hand_side == other.right_hand_side
+            and self.position_for_dot == other.position_for_dot
+        )
+
+    def __hash__(self):
+        return hash((self.left_hand_side, tuple(self.right_hand_side), self.position_for_dot))
